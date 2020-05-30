c033e758106a8bab94d880c9e0dfca308827b9b9
Alex <alex9999091@gmail.com>
2020-05-30 23:44:34 +0300

    Added docker deployment

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Contries from './Countries';
 
 function App() {
-  
-
-
 
   return (
     <div className="App">
